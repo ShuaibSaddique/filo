b4fa9605b31880eb8c908ea448bf38d96a9c0c2b
Shuaib Saddique <saddiqs1@aston.ac.uk>
2017-04-05 23:51:53 +0100

    refactor

--- anas_coursework/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-header('Refresh: 2; http://www.khana46.eas-cs2410-1516.aston.ac.uk/login.php');
-?>
--- coursework/locallogout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-header('Refresh: 2; http://localhost/projects/uni/coursework/login.php');
-?>
